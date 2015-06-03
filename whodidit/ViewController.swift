d5b64f7ac93cb08372f8963770f8791c56a4c371
curtisbacon <curtisbacon@MacBook-Pro.local>
2015-06-03 16:07:05 +0100

    Singleview App startup
    Creation of app project

---
+++ whodidit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whodidit
+//
+//  Created by Curtis Bacon on 03/06/2015.
+//  Copyright (c) 2015 Curtis Bacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
